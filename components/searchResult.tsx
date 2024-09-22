924064d9dc3193fcbda39a8642c23a294f0cee24
Ryan Dawes <dev@dawes.cc>
2024-09-22 19:52:57 -0400

    checkpoint at 2024-09-22 19:52:57-0400

--- components/searchResult.tsx
+++ components/searchResult.tsx
@@ -29,50 +29,37 @@ function formatName(name: string): string {
 }
 
 export default function SearchResult({ searchTerm, teams, conferences, onResultClick }: SearchResultsProps) {
+	if (!searchTerm) return null;
+
 	const filteredTeams = Object.values(teams).filter((team) => team.slug.toLowerCase().includes(searchTerm.toLowerCase()));
 
 	const filteredConferences = Object.values(conferences).filter((conference) =>
 		[conference.short, conference.full, conference.id].some((field) => field.toLowerCase().includes(searchTerm.toLowerCase()))
 	);
 
-	if (!searchTerm) {
-		return null;
-	}
+	const renderResults = (items: any[], type: string) => {
+		return items.map((item) => (
+			<Link
+				key={item.id}
+				href={`/${type}/${item.id}`}
+				className='flex items-center p-2'
+				onClick={onResultClick}>
+				<Image
+					src={type === "teams" ? `https://a.espncdn.com/i/teamlogos/ncaa/500/${item.id}.png` : item.href}
+					alt={`${item.slug || item.full} logo`}
+					height={50}
+					width={50}
+					className='size-10 mr-2'
+				/>
+				<span className='text-sm font-medium capitalize'>{formatName(item.slug || item.full)}</span>
+			</Link>
+		));
+	};
 
 	return (
 		<div className='flex flex-col space-y-2 p-2 max-h-full overflow-y-auto'>
-			{filteredTeams.map((team) => (
-				<Link
-					key={team.id}
-					href={`/teams/${team.id}`}
-					className='flex items-center p-2'
-					onClick={onResultClick}>
-					<Image
-						src={`https://a.espncdn.com/i/teamlogos/ncaa/500/${team.id}.png`}
-						alt={`${team.slug} logo`}
-						height={50}
-						width={50}
-						className='size-10 mr-2'
-					/>
-					<span className='text-sm font-medium capitalize'>{formatName(team.slug)}</span>
-				</Link>
-			))}
-			{filteredConferences.map((conference) => (
-				<Link
-					href={`/conferences/${conference.id}`}
-					key={conference.id}
-					className='flex items-center p-2'
-					onClick={onResultClick}>
-					<Image
-						src={conference.href}
-						alt={`${conference.full} logo`}
-						height={50}
-						width={50}
-						className='size-10 mr-2'
-					/>
-					<span className='text-sm font-medium capitalize'>{conference.full}</span>
-				</Link>
-			))}
+			{renderResults(filteredTeams, "teams")}
+			{renderResults(filteredConferences, "conferences")}
 			{filteredTeams.length === 0 && filteredConferences.length === 0 && <div className='p-2 text-gray-500'>No results found</div>}
 		</div>
 	);
