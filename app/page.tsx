4a6b9cc5df6e2c7d6e58619335833ff03b3189b0
Ryan Dawes <dev@dawes.cc>
2024-09-29 12:11:09 -0400

    checkpoint at 2024-09-29 12:11:09-0400

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Container } from "@/components/ui/container";
 export default async function Home() {
 	return (
 		<Container>
-			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
 				<div className='pt-4 h-fit'>
 					<Scoreboard />
 				</div>
